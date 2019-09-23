7bd279290e24e0404790d736d366d8b2b876ac20
Felix Stridsberg <felixstridsberg@gmail.com>
2019-09-23 20:11:25 +0200

    Dns message writer, write question.

--- src/main/kotlin/com/vadeen/dns/io/DnsMessageWriter.kt
+++ src/main/kotlin/com/vadeen/dns/io/DnsMessageWriter.kt
@@ -1,9 +1,22 @@
 package com.vadeen.dns.io
 
+import com.vadeen.dns.message.Question
 import com.vadeen.dns.message.record.Record
 
 class DnsMessageWriter(private val stream: DnsStreamWriter) {
 
+    /**
+     * Ref: https://tools.ietf.org/html/rfc1035#section-4.1.2
+     */
+    fun writeQuestion(question: Question) {
+        stream.writeLabels(question.name)
+        stream.writeShort(question.recordType.value)
+        stream.writeShort(question.recordClass.value)
+    }
+
+    /**
+     * Ref: https://tools.ietf.org/html/rfc1035#section-4.1.3
+     */
     fun writeRecord(record: Record) {
         stream.writeLabels(record.name)
         stream.writeShort(record.recordType.value)
--- src/test/kotlin/com/vadeen/dns/io/DnsMessageWriterTest.kt
+++ src/test/kotlin/com/vadeen/dns/io/DnsMessageWriterTest.kt
@@ -2,24 +2,50 @@ package com.vadeen.dns.io
 
 import com.vadeen.dns.constants.RecordClass
 import com.vadeen.dns.constants.RecordType
+import com.vadeen.dns.message.Question
 import com.vadeen.dns.message.record.UnknownRecord
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import java.io.ByteArrayOutputStream
 
 internal class DnsMessageWriterTest {
 
+    private var outputStream = ByteArrayOutputStream()
+    private var streamWriter = DnsStreamWriter(outputStream)
+    private var writer = DnsMessageWriter(streamWriter)
+
+    @BeforeEach
+    internal fun setUp() {
+        outputStream = ByteArrayOutputStream()
+        streamWriter = DnsStreamWriter(outputStream)
+        writer = DnsMessageWriter(streamWriter)
+    }
+
     @Test
-    fun writeRecord() {
+    internal fun testWriteQuestion() {
+        val name = listOf("ns".toByteArray(), "com".toByteArray())
+
+        val question = Question(name, RecordType.of(600), RecordClass.of(600))
+        writer.writeQuestion(question)
+
+        val expectedOutput = byteArrayOf(
+            0x02, 'n'.toByte(), 's'.toByte(), 0x03, 'c'.toByte(), 'o'.toByte(), 'm'.toByte(), 0x00,
+            0x02, 0x58,     // Type=600
+            0x02, 0x58      // Class=600
+        )
+        val output = outputStream.toByteArray()!!
+        assertEquals(expectedOutput.size, output.size)
+        assertTrue(output.contentEquals(expectedOutput))
+    }
+
+    @Test
+    internal fun testWriteRecord() {
         val name = listOf("ns".toByteArray(), "com".toByteArray())
         val data = byteArrayOf(0x01, 0x02, 0x03, 0x04, 0x05)
 
         val record = UnknownRecord(name, RecordType.of(600), RecordClass.of(600), 100, data)
-
-        val outputStream = ByteArrayOutputStream()
-        val streamWriter = DnsStreamWriter(outputStream)
-        val writer = DnsMessageWriter(streamWriter)
         writer.writeRecord(record)
 
         val expectedOutput = byteArrayOf(
