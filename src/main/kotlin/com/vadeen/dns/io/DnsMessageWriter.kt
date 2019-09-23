247fa351b45836fba983c88e3648edef7e8c41b2
Felix Stridsberg <felixstridsberg@gmail.com>
2019-09-23 20:34:11 +0200

    Write header.

--- src/main/kotlin/com/vadeen/dns/io/DnsMessageWriter.kt
+++ src/main/kotlin/com/vadeen/dns/io/DnsMessageWriter.kt
@@ -1,9 +1,23 @@
 package com.vadeen.dns.io
 
+import com.vadeen.dns.message.Header
 import com.vadeen.dns.message.Question
 import com.vadeen.dns.message.record.Record
 
 class DnsMessageWriter(private val stream: DnsStreamWriter) {
+
+    /**
+     * Ref: https://tools.ietf.org/html/rfc1035#section-4.1.1
+     */
+    fun writeHeader(header: Header) {
+        stream.writeShort(header.id)
+        stream.writeByte(getFlagByte1(header))
+        stream.writeByte(getFlagByte2(header))
+        stream.writeShort(header.questions)
+        stream.writeShort(header.answerRecords)
+        stream.writeShort(header.authorityRecords)
+        stream.writeShort(header.additionalRecords)
+    }
 
     /**
      * Ref: https://tools.ietf.org/html/rfc1035#section-4.1.2
@@ -26,5 +40,39 @@ class DnsMessageWriter(private val stream: DnsStreamWriter) {
         val data = record.toBytes()
         stream.writeShort(data.size)
         stream.writeBytes(data)
+    }
+
+    /**
+     * The byte containing QR, Opcode, AA, TC and RD.
+     */
+    private fun getFlagByte1(header: Header): Byte {
+        val opcode = header.operationCode.value.toInt()
+
+        var byte1 = ((opcode shl 3) and 0x78)
+        if (header.isResponse)
+            byte1 = byte1 or 0x80
+
+        if (header.authoritativeAnswer)
+            byte1 = byte1 or 0x04
+
+        if (header.truncation)
+            byte1 = byte1 or 0x02
+
+        if (header.recursionDesired)
+            byte1 = byte1 or 0x01
+
+        return byte1.toByte()
+    }
+
+    /**
+     * The byte containing RA and RCODE.
+     */
+    private fun getFlagByte2(header: Header): Byte {
+        var byte = header.responseCode.value.toInt() and 0x0F
+
+        if (header.recursionAvailable)
+            byte = byte or 0x80
+
+        return byte.toByte()
     }
 }
--- src/test/kotlin/com/vadeen/dns/io/DnsMessageWriterTest.kt
+++ src/test/kotlin/com/vadeen/dns/io/DnsMessageWriterTest.kt
@@ -1,7 +1,10 @@
 package com.vadeen.dns.io
 
+import com.vadeen.dns.constants.OperationCode
 import com.vadeen.dns.constants.RecordClass
 import com.vadeen.dns.constants.RecordType
+import com.vadeen.dns.constants.ResponseCode
+import com.vadeen.dns.message.Header
 import com.vadeen.dns.message.Question
 import com.vadeen.dns.message.record.UnknownRecord
 import org.junit.jupiter.api.Test
@@ -21,6 +24,64 @@ internal class DnsMessageWriterTest {
         outputStream = ByteArrayOutputStream()
         streamWriter = DnsStreamWriter(outputStream)
         writer = DnsMessageWriter(streamWriter)
+    }
+
+    @Test
+    internal fun testWriteHeader() {
+        val header = Header(
+            9999, true, OperationCode.Query(),
+            authoritativeAnswer = false,
+            truncation = true,
+            recursionDesired = false,
+            recursionAvailable = true,
+            responseCode = ResponseCode.NoError(),
+            questions = 1,
+            answerRecords = 2,
+            authorityRecords = 3,
+            additionalRecords = 4
+        )
+        writer.writeHeader(header)
+
+        val expectedOutput = byteArrayOf(
+            0x027, 0x0F,                    // ID = 9999
+            0x82.toByte(), 0x80.toByte(),   // Opcode, RCODE, flags.
+            0x00, 0x01,                     // Questions
+            0x00, 0x02,                     // Answer records
+            0x00, 0x03,                     // Authority records
+            0x00, 0x04                      // Additional records
+        )
+        val output = outputStream.toByteArray()!!
+        assertEquals(expectedOutput.size, output.size)
+        assertTrue(output.contentEquals(expectedOutput))
+    }
+
+    @Test
+    internal fun testWriteHeaderReversed() {
+        val header = Header(
+            9999, false, OperationCode.of(0xf),
+            authoritativeAnswer = true,
+            truncation = false,
+            recursionDesired = true,
+            recursionAvailable = false,
+            responseCode = ResponseCode.of(0xf),
+            questions = 4,
+            answerRecords = 3,
+            authorityRecords = 2,
+            additionalRecords = 1
+        )
+        writer.writeHeader(header)
+
+        val expectedOutput = byteArrayOf(
+            0x027, 0x0F,                    // ID = 9999
+            0x7D.toByte(), 0x0f.toByte(),   // Opcode, RCODE, flags.
+            0x00, 0x04,                     // Questions
+            0x00, 0x03,                     // Answer records
+            0x00, 0x02,                     // Authority records
+            0x00, 0x01                      // Additional records
+        )
+        val output = outputStream.toByteArray()!!
+        assertEquals(expectedOutput.size, output.size)
+        assertTrue(output.contentEquals(expectedOutput))
     }
 
     @Test
