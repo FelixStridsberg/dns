aa5530abced8dfeb6a228377c387d36ad19dc616
Felix Stridsberg <felixstridsberg@gmail.com>
2019-09-23 18:44:41 +0200

    Message to string.

--- src/main/kotlin/com/vadeen/dns/message/Message.kt
+++ src/main/kotlin/com/vadeen/dns/message/Message.kt
@@ -1,6 +1,7 @@
 package com.vadeen.dns.message
 
 import com.vadeen.dns.message.record.Record
+import kotlin.text.StringBuilder
 
 data class Message(
     val header: Header,
@@ -8,4 +9,33 @@ data class Message(
     val answerRecords: List<Record>,
     val authorityRecords: List<Record>,
     val additionalRecords: List<Record>
-)
+) {
+
+    override fun toString(): String {
+        val result = StringBuilder()
+        result.append(header)
+
+        if (questions.isNotEmpty()) {
+            result.append("\n\nQuestions:\n")
+                .append(questions.joinToString(separator = "\n"))
+        }
+
+        if (answerRecords.isNotEmpty()) {
+            result.append("\n\nAnswers:\n")
+                .append(answerRecords.joinToString(separator = "\n"))
+        }
+
+        if (authorityRecords.isNotEmpty()) {
+            result.append("\n\nAuthorities:\n")
+                .append(authorityRecords.joinToString(separator = "\n"))
+        }
+
+        if (additionalRecords.isNotEmpty()) {
+            result.append("\n\nAdditional:\n")
+                .append(additionalRecords.joinToString(separator = "\n"))
+        }
+
+        result.append('\n')
+        return result.toString()
+    }
+}
---
+++ src/test/kotlin/com/vadeen/dns/message/MessageTest.kt
@@ -0,0 +1,62 @@
+package com.vadeen.dns.message
+
+import com.vadeen.dns.constants.OperationCode
+import com.vadeen.dns.constants.RecordClass
+import com.vadeen.dns.constants.RecordType
+import com.vadeen.dns.constants.ResponseCode
+import com.vadeen.dns.message.record.ARecord
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class MessageTest {
+
+    @Test
+    fun testToString() {
+        var header = Header(
+            54321, true, OperationCode.Query(),
+            false, false, true, true,
+            ResponseCode.NoError(), 1, 2, 3, 4)
+        val name = listOf("ns".toByteArray(), "vadeen".toByteArray(), "com".toByteArray())
+        val question = Question(name, RecordType.A(), RecordClass.IN())
+
+        val ip = byteArrayOf(0x0A, 0x01, 0x01, 0xFF.toByte())
+        val record = ARecord.of(name, RecordType.A(), RecordClass.IN(), 5444, ip)
+
+        val message = Message(header, listOf(question), listOf(record), listOf(record), listOf(record))
+
+        assertEquals(
+            "Header (id: 54321, opcode: QUERY, qr: true, aa: false, tc: false, rc: true, ra: true, rcode: NOERR)\n" +
+                    "questions: 1, answers: 2, authorities: 3, additionals: 4\n" +
+                    "\n" +
+                    "Questions:\n" +
+                    "ns.vadeen.com                        IN         A\n" +
+                    "\n" +
+                    "Answers:\n" +
+                    "ns.vadeen.com             5444       IN         A          10.1.1.255\n" +
+                    "\n" +
+                    "Authorities:\n" +
+                    "ns.vadeen.com             5444       IN         A          10.1.1.255\n" +
+                    "\n" +
+                    "Additional:\n" +
+                    "ns.vadeen.com             5444       IN         A          10.1.1.255\n",
+                    message.toString()
+        )
+    }
+
+    @Test
+    fun testToStringNoRecords() {
+        var header = Header(
+            54321, true, OperationCode.Query(),
+            false, false, true, true,
+            ResponseCode.NoError(), 1, 2, 3, 4)
+
+        val message = Message(header, emptyList(), emptyList(), emptyList(), emptyList())
+
+        assertEquals(
+            "Header (id: 54321, opcode: QUERY, qr: true, aa: false, tc: false, rc: true, ra: true, rcode: NOERR)\n" +
+                    "questions: 1, answers: 2, authorities: 3, additionals: 4\n",
+            message.toString()
+        )
+    }
+}
