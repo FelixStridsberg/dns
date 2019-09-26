768b860b42beec6d646b7d2f7bedefa6afc62ca2
Felix Stridsberg <felixstridsberg@gmail.com>
2019-09-26 20:15:02 +0200

    Test coverage.

--- src/main/kotlin/com/vadeen/dns/message/Question.kt
+++ src/main/kotlin/com/vadeen/dns/message/Question.kt
@@ -14,7 +14,8 @@ data class Question(
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
-        if (javaClass != other?.javaClass) return false
+        if (other == null) return false
+        if (javaClass != other.javaClass) return false
 
         other as Question
 
